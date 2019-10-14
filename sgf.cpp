e8c0ae7ef9575ae17a5bf724b91075697f825f9e
Mahiransh Sharma <51320070+mahisharma-cs@users.noreply.github.com>
2019-10-14 23:01:31 +0530

    Add files via upload

---
+++ sgf.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int x;
+//	cin>>x;
+	switch(002){
+		case 1:cout<<"case 1";
+		break;
+		case 2:cout<<"case 2";
+		break;
+		case 3:cout<<"case 3";
+		break;
+		case 4:cout<<"case 4";
+		break;
+		default:cout<<"default case";
+		break;
+	}
+	return 0;
+}
